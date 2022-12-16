d64b6508b391f3adb4be439ecfe373baf2df8d46
Raven Blackhart <raven@cafiend.com>
2022-12-16 10:41:11 +0100

    Build

---
+++ Assets/Scripts/Helpers/Quit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame(); 
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+}
+}
